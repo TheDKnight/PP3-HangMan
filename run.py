c56d8fdb262c1f81209965ad4e5fc496582bc962
TheDKnight <113733655@umail.ucc.ie>
2022-10-30 22:42:48 +0000

    add words to list and set lines as random word from list using random function

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+f = open('words.txt', 'r')
+lines = random.choice(f.readlines() )
+f.close()
+print(lines)
