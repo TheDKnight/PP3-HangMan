88871b0a992ce81bd557378ee02c970188899e10
TheDKnight <113733655@umail.ucc.ie>
2022-11-01 22:34:08 +0000

    added the play again function to allow players to have another game if they win or lose.

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def welcome() :
     name = input("Welcome to PP3 Hangman Game Please Enter Your Name Below :\n Name: ")
     while name.isalpha() == False :
         print("Please only enter alphabetic letters for your name") 
-        name = input("Please enter your name again: ")
+        #name = input("Please enter your name again: ")
     else: 
         print(f"Welcome {name}, You will be playing against the computer today from a randomly selected word list.")
     
@@ -29,23 +29,31 @@ def randomWord():
     
     return(word)
 
-
+def playAgain():
+    play = input("\nGame is finished, would you like to play another game?\n\npress y for another game or n to exit \n").lower()
+        
+    if len(play) != 1 or not play.isalpha():
+        print("Please enter either a y or ")
+    elif play == "y" :
+        main()
+    elif play == "n": 
+        exit()
+    return(playAgain())
 def main():
    # welcome()
     gameWord = randomWord()
     lenGameWord = len(gameWord) 
-    print(gameWord)
-    print(f"The word is {lenGameWord} Characters Long")
+    #print(gameWord)
+    print(f"\nThe word is {lenGameWord} Characters Long")
     guessed_letters = []
     
     blanks = len(gameWord) * '_'
-    #print(blanks)
     blanks = list(blanks)
     
     
     lives = 5
     while lives > 0:
-        guess = input("Enter A Letter \n".lower()) 
+        guess = input("Enter A Letter \n").lower()
         if len(guess) != 1 or not guess.isalpha():
             print("Please enter a correct value")
            # guess = input("Enter A Letter \n".lower()) 
@@ -55,22 +63,28 @@ def main():
         elif guess not in gameWord :
             lives -= 1
             guessed_letters.append(guess)
-            print(f"{guess} Is not in the selcted word" )
-            print(f"You have {lives} lives left")   
+            print(f"{guess} Is not in the selcted word You have {lives} lives left")  
+            print(''.join(blanks)) 
         elif guess in gameWord:
            for i in gameWord:
                for i, char in enumerate(gameWord):
                    if char == guess:
                       blanks[i] = guess
+           guessed_letters.append(guess)
+           print(F"{guess} is in the word")
            print(''.join(blanks))
+           
         print(f"Letters Guessed Allready {guessed_letters}")
 
         if ''.join(blanks) == gameWord:
             print(f'You won. The secret word was: {gameWord}.')
+            playAgain()
         
     else:
-        print("you lose")
-        print(f'The secret word was: {gameWord}.')
+        print("")
+        print(f'you lose \n The secret word was: {gameWord}.')
+        playAgain()
+
 main()
 #print(randomWord())
 #print(len(randomWord()))
